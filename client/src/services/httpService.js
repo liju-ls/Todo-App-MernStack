2bad8000952c797477cb78e6ad4ebf838e7bbeb3
Liju <liju.lukas@gmail.com>
2023-11-07 19:46:28 +0530

    update

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://todo-app-mern-stack-eight.vercel.app/",
 });
 
 export default http;
